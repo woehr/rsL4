3e861c58c91e4ac779c6d41ed726ce053e4f3298
Jordan Woehr <jordanwoehr@gmail.com>
2014-11-18 14:38:06 -0700

    Incorporate debug output when the stack overflows.

--- librsL4-boot/lib.rs
+++ librsL4-boot/lib.rs
@@ -4,21 +4,32 @@
 #![feature(lang_items)]
 
 extern crate core;
-use core::intrinsics::{volatile_load, volatile_store};
 
-// When setting up the stack we make sure there is always room to call putc for debuggin purposes.
-#[no_stack_check]
-fn putc(c: char) {
+use core::intrinsics::{volatile_load, volatile_store};
+use core::str::StrPrelude;
+
+#[allow(dead_code)]
+extern {
+  fn increase_stack();
+}
+
+fn put_c(c: u8) {
   let thr: * mut u8 = 0x44E09000 as * mut u8;
   let sta: * const u8 = (0x44E09014) as * const u8;
   unsafe {
     while volatile_load(sta) & 0x20 == 0 {}
-    volatile_store(thr, c as u8);
+    volatile_store(thr, c);
+  }
+}
+
+fn put_str(s: &str) {
+  for b in s.bytes() {
+    put_c(b);
   }
 }
 
 fn recurse() {
-  putc('a');
+  put_str("a");
   recurse();
 }
 
@@ -35,7 +46,10 @@ pub extern fn main() {
 #[lang = "eh_personality"]  extern fn eh_personality() {}
 
 // This function will only be called if a call to panic! is made.
-#[lang = "panic_fmt"]              fn panic_fmt() -> ! { loop {} }
+#[lang = "panic_fmt"]              fn panic_fmt() -> ! {
+  put_str("panic_fmt called");
+  loop {}
+}
 
 // LLVM checks every function entry to make sure enough stack is allocated, if it isn't this
 // function is called. This is normally used in a split stack situation to allocate more stack
@@ -44,8 +58,13 @@ pub extern fn main() {
 // calling __morestack from running out of stack while executing __morestack.
 #[no_mangle]
 #[no_stack_check]
-extern fn __morestack() {
-  putc('m');
+fn __morestack() {
+  // Increase the stack limit so we can safely call into stack allocating functions. This does not
+  // overflow the stack since space was left at the end of the stack for this purpose.
+  unsafe {
+    increase_stack();
+  }
+  put_str("Stack overflow");
   loop {}
 }
 
--- rsL4-boot/start.s
+++ rsL4-boot/start.s
@@ -1,22 +1,48 @@
 	.text
 	.syntax unified
 	.cpu	cortex-a8
+	.eabi_attribute	6, 10
+	.eabi_attribute	7, 65
+	.eabi_attribute	8, 1
+	.eabi_attribute	9, 2
+	.fpu	neon
+	.eabi_attribute	15, 1
+	.eabi_attribute	16, 1
+	.eabi_attribute	17, 2
+	.eabi_attribute	20, 1
+	.eabi_attribute	21, 1
+	.eabi_attribute	23, 3
+	.eabi_attribute	24, 1
+	.eabi_attribute	25, 1
+	.eabi_attribute	14, 0
+	.eabi_attribute	68, 1
 
+# Bootloader entry point. This function sets up the stack and stack limit then branches to main
 	.globl	start
 	.align	2
 start:
 # Setup our stack
 	ldr sp, =STACK_START
-# Allocate 8 bytes for llvm to use for the stack limit
-	sub sp, #8
-# Place the stack limit where llvm expects to find it
+# Set the stack limit
 	ldr r0, =STACK_LIMIT
-	str r0, [sp, 4]
-# Set the 8 allocated bytes to be the TCB
-	mcr p15, #0, sp, c13, c0, #3
+	ldr r1, =p_sl
+	str r0, [r1]
+# Set the TCB to the 8 bytes set aside in the linker script
+	ldr r0, =p_tcb
+	mcr p15, #0, r0, c13, c0, #3
 # Branch to the Rust entry point
 	bl main
 # Should never get here
 hang:
 	b hang
+
+# Increases the stack limit to the maximum allowed. This function is used when __morestack needs
+# stack for debug output.
+	.globl	increase_stack
+	.align	2
+increase_stack:
+	ldr r0, =STACK_END
+	ldr r1, =p_sl
+	str r0, [r1]
+	mov pc, lr
 
