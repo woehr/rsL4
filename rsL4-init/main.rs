8ae65366a39e4ea2af4e01c6143ac28e7ef6b7d5
Jordan Woehr <jordanwoehr@gmail.com>
2015-01-29 11:17:57 -0700

    Removed dead code

--- rsL4-init/main.rs
+++ rsL4-init/main.rs
@@ -1,68 +1,13 @@
 #![crate_name = "rsL4-init"]
 #![crate_type = "bin"]
 #![no_std]
-#![feature(lang_items)]
+#![feature(asm, lang_items, start)]
 
 #[macro_use]
 extern crate core;
 
-/*
-use core::prelude;
-use core::fmt;
-use core::intrinsics::{volatile_load, volatile_store};
-use core::result::{Ok};
-
-struct Uart {
-  thr: * mut u8,
-  sta: * const u8,
-}
-
-impl fmt::FormatWriter for Uart {
-  fn write(&mut self, bytes: &[u8]) -> fmt::Result {
-    for &b in bytes.iter() {
-      unsafe {
-        while volatile_load(self.sta) & 0x20 == 0 {}
-        volatile_store(self.thr, b);
-      }
-    }
-    Ok(())
-  }
-}
-
-static mut uart0: Uart = Uart {thr: 0x44E09000 as * mut u8, sta: 0x44E09014 as * const u8};
-*/
-
-/*
-fn put_c(c: u8) {
-  let thr: * mut u8 = 0x44E09000 as * mut u8;
-  let sta: * const u8 = (0x44E09014) as * const u8;
-  unsafe {
-    while volatile_load(sta) & 0x20 == 0 {}
-    volatile_store(thr, c);
-  }
-}
-
-fn put_str(s: &str) {
-  for b in s.bytes() {
-    put_c(b);
-  }
-}
-*/
-
-/*
-fn recurse(call_count: u32) {
-//  fmt::write(uart0, "{},", call_count);
-  recurse(call_count + 1);
-}
-*/
-
-//#[no_mangle]
-//#[allow(dead_code)]
-//pub extern fn main(argc: u32, argv: * const * const u8) -> ! {
 #[start]
 pub fn rsl4_init(_: isize, _: * const * const u8) -> isize {
-//  write!(&mut uart0, "There were {} args.\n", argc as u8);
-//  recurse(0);
   loop {}
   0
 }
